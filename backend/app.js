4b3db031f9052b6f099254ef5891200eedabcda9
emely <20230621@ricaldone.edu.sv>
2025-02-28 09:49:36 -0600

    Primer MERN

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+//importo tofo lo de la libreria de Express
+import express from "express";
+
+//creo una constante que es igual a la libreria que importé
+const app = express();
+
+//exporto la constante para poder uar express en otros archivos
+export default app;
---
+++ backend/database.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose";
+
+//1- Confuguro la URI o dirección de la base de datos
+const URI = "mongodb://localhost:27017/ZonaDigitalDB20230621";
+
+//- Conecto la base de datos 
+mongoose.connect(URI);
+
+// Comprobar  que todo funciona
+
+//3- Creo una constante que es igual a la conexión
+const connection = mongoose.connection;
+
+// veo si funciona
+connection.once("open", () => {
+    console.log("DB is connected");
+});
+
+// veo si desconectó
+connection.on("Disconnected", () => {
+    console.log("DB is disconnected");
+});
+
+//veo si hay un error
+connection.on("error", (error) => {
+    console.log("error found" + error);
+});
+
+
