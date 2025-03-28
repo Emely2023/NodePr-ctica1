e6b420c65383d38d4c63797a5afb010abeefc2dc
20230621@ricaldone.edu.sv <20230621@ricaldone.edu.sv>
2025-03-28 08:25:40 -0600

    login22222222222

--- backend/src/controllers/loginControllers.js
+++ backend/src/controllers/loginControllers.js
@@ -20,9 +20,60 @@ loginController.login = async(req,res)=> {
         let userFound;// Guarda el usuario encontrado 
         let userType; // Guarda el tipo de usuario
 
+
+
         //1. Admin
-    }catch (error){
+
+        if(email === config.ADMIN.emailAdmin && password === config.ADMIN.password){
+            userType= "admin";
+            userFound= {_id: "admin"};
+        }else{
+            //2.Empleados
+            userFound = await Empleados.findOne({email});
+            userType = "employee"
+            if(!userFound){
+                //3. Cliente
+                userFound = await Clientes.findOne({email});
+                userType = "customer"
+                
+            }
+        }
+
+    //Si no encontramos a ningun usuario con esas credenciales
+    if(!userFound){
+        return res.json({ message: "User not found"});
+    }
+
+    //Validar la contraseña
+    //SOLO SI NO ES ADMIN
+    if(userType !== "admin"){
+        const isMatch = await bcryptjs.compare(password, userFound.password)
+        if(!isMatch){
+            return res.json({message: "Invalid password"})
+        }
+    }
+
+    //TOKEN 
+
+    //PARA VALIDAR
+    jsonwebtoken.sign(
+        //1-Que voy a guardar
+        {id: userFound._id, userType},
+        //Secreto
+        config.JWT.secret,
+        //cuando expira
+        {expiresIn: config.JWT.expiresIn},
+        //4. Funcion Flecha
+        (error, token)=>{
+            if(error) console.log("error"+ error)
+                res.cookie("authCookie", token)
+            res.json({message: "Login successfull"})
+        }
+    )
         
+           
+    }catch (error){
+
     }
 
 }
